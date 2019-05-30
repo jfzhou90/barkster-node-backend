7eedc488dbc20a17dc097bb622f998a52fdc68f1
Joey Zhou <jfzhou.nc@gmail.com>
2019-05-29 20:04:08 -0400

    fix adding sessions to mongoose

--- models/DogPark.js
+++ models/DogPark.js
@@ -1,21 +1,25 @@
 import mongoose, { Schema } from 'mongoose';
 
-const dogParkSchema = new Schema({
-  parkName: { type: String, unique: true, required: true },
-  address: { type: String, unique: true, required: true },
-  city: { type: String, required: true },
-  zipCode: {type: Number, required: true},
-  hours: Object,
-  currentDogCount: { type: Number, default: 0 },
-  dateCreated: { type: Date, default: Date.now },
-  lastUpdated: { type: Date, default: Date.now },
-  sessions: {
-    '1800000': { type: Object, default: {} },
-    '3600000': { type: Object, default: {} },
-    '5400000': { type: Object, default: {} },
-    '7200000': { type: Object, default: {} },
+const dogParkSchema = new Schema(
+  {
+    parkName: { type: String, unique: true, required: true },
+    address: { type: String, unique: true, required: true },
+    city: { type: String, required: true },
+    zipCode: { type: Number, required: true },
+    hours: Object,
+    currentDogCount: { type: Number, default: 0 },
+    sessions: {
+      type: Object,
+      default: {
+        '1800000': { type: Map, of: Number },
+        '3600000': { type: Map, of: Number },
+        '5400000': { type: Map, of: Number },
+        '7200000': { type: Map, of: Number },
+      },
+    },
   },
-});
+  { strict: false, timestamps: { createdAt: 'created_at' } }
+);
 
 dogParkSchema.methods.getCurrentNumOfDogs = function() {
   this.calculateSessions();
@@ -25,7 +29,7 @@ dogParkSchema.methods.getCurrentNumOfDogs = function() {
 dogParkSchema.methods.addSession = function(sessionType, incomingDogsCount) {
   if (!sessionType || !this.sessions[sessionType]) return;
 
-  this.sessions[sessionType][Date.now()] = parseInt(incomingDogsCount);
+  this.set(`sessions.${sessionType}.${Date.now()}`, parseInt(incomingDogsCount))
   return this;
 };
 
@@ -33,6 +37,7 @@ dogParkSchema.methods.calculateSessions = function() {
   let numberOfDogs = 0;
   Object.keys(this.sessions).forEach(duration => {
     const sessions = this.sessions[duration];
+
     Object.keys(sessions).forEach(session => {
       Date.now() - session >= duration
         ? delete sessions[session]
--- routes/parkRoutes.js
+++ routes/parkRoutes.js
@@ -20,18 +20,20 @@ router.get('/:id', (request, response) => {
   DogPark.findById(request.params.id, function(error, dogPark) {
     if (error) return response.status(500).send(error);
     if (!dogPark) return response.status(404).send({Error:"Not Found"});
-    dogPark.calculateSessions().save();
+    // dogPark.calculateSessions().save();
     response.send(dogPark);
   });
 });
 
 router.post('/:id/checkIn', (request,response) => {
-  DogPark.findById(request.params.id, function(error, dogPark) {
+  DogPark.findById(request.params.id, function(error, doc) {
     if (error) return response.status(500).send(error);
-    if (!dogPark) return response.status(404).send({Error:"Not Found"});
-  
-    dogPark.addSession(request.body.duration, request.body.count).calculateSessions().save();
-    response.send({currentDogCount:dogPark.currentDogCount});
+    if (!doc) return response.status(404).send({Error:"Not Found"});
+    
+    console.log(doc);
+    doc.addSession(request.body.duration, request.body.count).calculateSessions().save();
+    console.log(doc);
+    response.send({currentDogCount:doc.currentDogCount});
   });
 })
 
